89cab81918fa0ba25224959e26206b492ce3ef30
Selsam <selvi.viswalingam@gmail.com>
2019-06-28 10:39:01 -0400

    This is the first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAPpAndroid()
+	{
+		System.out.println("AppiumAutomation");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("selviIOSApps");	
+	}
+
+}
